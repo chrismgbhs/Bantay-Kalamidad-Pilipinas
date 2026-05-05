901edffdd6509daf21f168c8027973edd17ac046
Egglord0808 <lyuy707@gmail.com>
2026-05-05 11:01:12 +0700

    minor edits

--- Bantay Kalamidad Pilipinas/View/rescue_login_view.xaml.cs
+++ Bantay Kalamidad Pilipinas/View/rescue_login_view.xaml.cs
@@ -24,5 +24,10 @@ namespace Bantay_Kalamidad_Pilipinas.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
