9371f6445178fc1f335c11a370a16e2378600547
Egglord0808 <lyuy707@gmail.com>
2026-05-11 09:18:35 +0700

    chris minor updates

--- Bantay Kalamidad Pilipinas/ViewModel/admin_login_ViewModel.cs
+++ Bantay Kalamidad Pilipinas/ViewModel/admin_login_ViewModel.cs
@@ -6,6 +6,7 @@ using Bantay_Kalamidad_Pilipinas.Model;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Forms;
+using System.Windows;
 
 namespace Bantay_Kalamidad_Pilipinas.ViewModel
 {
@@ -13,17 +14,28 @@ namespace Bantay_Kalamidad_Pilipinas.ViewModel
     {
         public static UserModel CurrentUser { get; set; }
         public ICommand LoginCommand { get; set; }
+        public ICommand BackCommand { get; set; }
 
         public admin_login_ViewModel()
         {
             CurrentUser = new UserModel();
             LoginCommand = new RelayCommand(Login);
+            BackCommand = new RelayCommand(Back);
         }
 
         private async void Login()
         {
             //MessageBox.Show($"username: {CurrentUser.Username}, password: {CurrentUser.Password}");
             await DatabaseManager.Login(CurrentUser, "admin", "Admin");
+        }
+
+        private void Back()
+        {
+            var window = new Window();
+            window = new View.start_view();
+            window.Show();
+            System.Windows.Application.Current.MainWindow.Close();
+            System.Windows.Application.Current.MainWindow = window;
         }
     }
 }
